ad1085896832b4d1d0a4345d04a5dfa609237763
pedrosantos13s <pedro_santos_13s@hotmail.com>
2019-12-11 10:32:52 -0300

    Terminado

--- cep.py
+++ cep.py
@@ -2,8 +2,13 @@ import requests
 def qualCEP (cep):
     response = requests.get('https://viacep.com.br/ws/' + str(cep) + '/json' )
     infos = response.json()
-    print(response.text)
+    print ('CEP: ', infos['cep'])
+    print ('COMP: ', infos['complemento'])
+    print ('BAIRRO: ', infos['bairro'])
+    print ('CIDADE: ', infos['localidade'])
+    print ('UF: ', infos['uf'])
+    #print(response.text)
     return infos
-print('Digite o CEP: ')
-cepe = input()
+#print('Digite o CEP: ')
+cepe = input('Digite o CEP: ')
 qualCEP(cepe)
