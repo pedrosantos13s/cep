929d16f5eb59326fe6a213de1c948ed4e4e567d1
ricarthlima <rrsl@cin.ufpe.br>
2019-12-16 15:35:33 -0300

    janela x implementada

--- udp-server.py
+++ udp-server.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from _socket import *
 import json
+import matplotlib.axes as axes
 
 def convert_str_json(str_dic):
     return json.loads(str_dic)
@@ -26,7 +27,7 @@ x = []
 y = []
 i = 0
 janela = 10
-
+ax.set_xlim(0, janela)
 while True:
     # Receber resposta
     print("> Esperando resposta...")
@@ -34,20 +35,26 @@ while True:
 
     json_dic = convert_str_json(msg)
 
+    # i += 1
+    # if len(y) >= janela:
+    #     x.pop(0)
+    #     y.pop(0)
+
     x.append(i)
     y.append(json_dic["temperatura_externa"])
 
+    
+    # 
     ax.plot(x, y, color='b')
-
+    
     fig.canvas.draw()
-
+    
     # print("Mensagem recebida de: ", str(adr))
     # print(msg.decode())
 
     i += 1
     if len(y) >= janela:
-        x.pop(0)
-        y.pop(0)
+        ax.set_xlim(i-janela, i)
 
     #msg = input("O que deseja enviar?")
     #msg = msg.encode()
