a9ddddd995a8c00042c5c387a7f76a7e0e9ecb8c
felipefalcantara <felipefr.alcantara@gmail.com>
2019-12-16 16:07:52 -0300

    4 plots funcionando

--- udp-server.py
+++ udp-server.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from _socket import *
 import json
 
+
 def convert_str_json(str_dic):
     return json.loads(str_dic)
 
@@ -11,45 +12,56 @@ UDP = SOCK_DGRAM
 TCP = SOCK_STREAM
 
 skt = socket(IPV4, UDP)
-
 porta = int(input("Qual porta deseja bindar? "))
-
-skt.bind(('',porta))
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-fig.show()
-
-
+skt.bind(('', porta))
 
 x = []
-y = []
+y1 = []
+y2 = []
+y3 = []
+y4 = []
+y5 = []
 i = 0
 janela = 10
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(2, 2, 1)
+ax1.set_xlim(0, janela+1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax2.set_xlim(0, janela+1)
+ax3 = fig.add_subplot(2, 2, 3)
+ax3.set_xlim(0, janela+1)
+ax4 = fig.add_subplot(2, 2, 4)
+ax4.set_xlim(0, janela+1)
+
+fig.show()
+ax1.set_xlim(0, janela+1)
 
 while True:
     # Receber resposta
     print("> Esperando resposta...")
     msg, adr = skt.recvfrom(1024)
-
     json_dic = convert_str_json(msg)
 
     x.append(i)
-    y.append(json_dic["temperatura_extena"])
+    y1.append(json_dic["temperatura_externa"])
+    y2.append(json_dic["temperatura_interna"])
+    y3.append(json_dic["vcc"])
+    y4.append(json_dic["temperatura_setada"])
+    y5.append(json_dic["viscosidade_fluido"])
 
-    ax.plot(x, y, color='b')
-
+    ax1.plot(x, y1, color='b')
+    ax2.plot(x, y2, color='b')
+    ax3.plot(x, y3, color='b')
+    ax4.plot(x, y4, color='b')
+    
     fig.canvas.draw()
 
-    # print("Mensagem recebida de: ", str(adr))
-    # print(msg.decode())
-
     i += 1
-    if len(y) >= janela:
-        x.pop(0)
-        y.pop(0)
-
-    #msg = input("O que deseja enviar?")
-    #msg = msg.encode()
-    # Enviar mensagem
-    #skt.sendto(msg, adr)
+    if i > janela:
+        ax1.set_xlim(i-janela, i+1)
+        ax2.set_xlim(i-janela, i+1)
+        ax3.set_xlim(i-janela, i+1)
+        ax4.set_xlim(i-janela, i+1)
+        
