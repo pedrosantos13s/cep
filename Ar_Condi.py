03d56ae5cc21b24dc5096c4fce82a292029363af
felipefalcantara <felipefr.alcantara@gmail.com>
2019-12-16 14:47:00 -0300

    bonitinho

--- Ar_Condi.py
+++ Ar_Condi.py
@@ -15,11 +15,13 @@ def ar_val():
     while vcc > 25 or vcc < 8:
         vcc = numpy.random.normal(12,4.33,1)  
 
-    temp_Set = temp_Ext * 3
-    while temp_Set > 2.2*temp_Ext or temp_Set < 0.3*temp_Ext:
-        temp_Set = numpy.random.normal(temp_Ext,0.4*temp_Ext,1)
+    temp_Mult = 3
+    while temp_Mult > 2.2 or temp_Mult < 0.3:
+        temp_Mult = numpy.random.normal(1,0.4,1)
+    
+    temp_Set = temp_Ext * temp_Mult
 
-    visc_Flu = numpy.random.normal(0.001002,0.0001002,1)  
+    visc_Flu = numpy.random.normal(0.001002,0.0001002/3,1)
 
     dic = {}
     dic["temperatura_externa"] = temp_Ext[0]
@@ -35,20 +37,22 @@ def ar_msg():
     dic = ar_val()
     encoder = json.JSONEncoder()
     str_dic = encoder.encode(dic)
-    print(type(str_dic))
+    # print(type(str_dic))
     print(str_dic)
     msg = str_dic.encode()
-    skt.sendto(msg,(ip,porta))
+    return msg
+    # skt.sendto(msg,(ip,porta))
 
 def ar_cond():
-
-    ar_msg()
-    Timer(5, ar_cond).start()
+    t = 5
+    msg = ar_msg()
+    skt.sendto(msg,(ip,porta))
+    Timer(t, ar_cond).start()
 
     
 IPV4 = AF_INET
 UDP = SOCK_DGRAM
-TCP = SOCK_STREAM
+# TCP = SOCK_STREAM
 
 skt = socket (IPV4, UDP)
 
